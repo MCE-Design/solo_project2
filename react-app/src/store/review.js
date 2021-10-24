5de5f609dec3f582cd33c00bd7b036928376c7bd
Michael E <mceng.design@gmail.com>
2021-10-24 02:20:37 -0700

    Added basic frontend route for Review Edit

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -21,7 +21,6 @@ export const getReview = (id) => async dispatch => {
 }
 
 export const newReview = (review) => async dispatch => {
-  console.log(review)
   const response = await fetch(`/api/review/`,
     {
       method: "POST",
@@ -36,6 +35,22 @@ export const newReview = (review) => async dispatch => {
     console.log(data)
     dispatch(load(data))
   } else return "Thunk Error: Review Submit Failed"
+}
+
+export const editReview = (review) => async dispatch => {
+  console.log(review)
+  const response = await fetch(`/api/review/`,
+    {
+      method: "PUT",
+      body: review
+    }
+  )
+  if (response.ok) {
+    const data = await response.json()
+    console.log("OK")
+    console.log(data)
+    dispatch(load(data))
+  } else return "Thunk Error: Review Edit Failed"
 }
 
 export default function reducer(state = initialState, action) {
