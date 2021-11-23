37fd23a4fb11e35b66b03ab43e34bdd5ccf82a8c
Michael E <mceng.design@gmail.com>
2021-11-23 09:44:22 -0700

    Changed out old URLs in seeders for S3 URLs

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -3,10 +3,10 @@ from app.models import db, Image
 
 # Adds a demo images, you can add other images here if you want
 def seed_images():
-    image1 = Image(userId=2, imageable_id=1, imageable_type='business', imageUrl='http://www.threepanelsoul.com/comics/2011-05-23-195.png')
-    image2 = Image(userId=3, imageable_id=2, imageable_type='business', imageUrl='http://www.threepanelsoul.com/comics/2013-04-15-273.png')
-    image3 = Image(userId=2, imageable_id=1, imageable_type='review', imageUrl='http://www.threepanelsoul.com/comics/2015-03-16-362.png')
-    image4 = Image(userId=1, imageable_id=2, imageable_type='review', imageUrl='http://www.threepanelsoul.com/comics/2013-12-16-305.png')
+    image1 = Image(userId=2, imageable_id=1, imageable_type='business', imageUrl='https://yap-bucket.s3.us-west-1.amazonaws.com/be6f0d96dff54c32b7a3a61dfe30c424.jpg')
+    image2 = Image(userId=3, imageable_id=2, imageable_type='business', imageUrl='https://yap-bucket.s3.us-west-1.amazonaws.com/687fc9c425ad4f4185370715c54b7f16.jpg')
+    image3 = Image(userId=2, imageable_id=1, imageable_type='review', imageUrl='https://yap-bucket.s3.us-west-1.amazonaws.com/bb77393ea08441e2bdff58c693df13ff.png')
+    image4 = Image(userId=1, imageable_id=2, imageable_type='review', imageUrl='https://yap-bucket.s3.us-west-1.amazonaws.com/b06e2e0e81e14418846e2d4e845fefe7.jpg')
     image5 = Image(userId=1, imageable_id=1, imageable_type='user', imageUrl='https://yap-bucket.s3.us-west-1.amazonaws.com/c69b2f330aa54937adc60fecf2f732b0.jpg', imageCaption="Just hangin'")
 
     db.session.add(image1)
